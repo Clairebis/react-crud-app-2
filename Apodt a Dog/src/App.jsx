21290b46421d715376df9613c9494c5f4964def5
Claire <eaaclhb@students.eaaa.dk>
2023-10-05 12:24:49 +0200

    update

--- Apodt a Dog/src/App.jsx
+++ Apodt a Dog/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Nav from "./components/Nav";
+import DogList from "./pages/DogList";
+import NewDog from "./pages/NewDog";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<DogList />} />
+          <Route path="/add" element={<NewDog />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
