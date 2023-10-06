059f9d78ea94f2a9d94af6d2030342ffcba59389
Claire <eaaclhb@students.eaaa.dk>
2023-10-06 14:11:18 +0200

    Update DogDetails.jsx

--- Apodt a Dog/src/pages/DogDetails.jsx
+++ Apodt a Dog/src/pages/DogDetails.jsx
@@ -1,9 +1,36 @@
+import { useNavigate, useParams } from "react-router-dom";
 import User from "../components/User";
+import { useEffect, useState } from "react";
 
-export default function DogDetails({ dog }) {
+export default function DogDetails() {
+  const { dogId } = useParams();
+  const navigate = useNavigate();
+  const url = `https://dog-app-7792f-default-rtdb.europe-west1.firebasedatabase.app/dogs/${dogId}.json`;
+
+  const [dog, setDog] = useState({
+    name: "",
+    breed: "",
+    age: "",
+    description: "",
+    image: "",
+    contact: "",
+    status: "",
+    uid: "",
+  });
+
+  useEffect(() => {
+    //fetch existing post data based on the dogId
+    async function getDog() {
+      const response = await fetch(url);
+      const dogData = await response.json();
+      setDog(dogData);
+    }
+    getDog();
+  }, [url]);
+
   return (
     <>
-      <h1>Read all about: {dog.name}</h1>
+      <h1>Read all about {dog.name}</h1>
       <article>
         <User uid={dog.uid} />
         <img src={dog.image} alt={dog.name} />
@@ -11,6 +38,9 @@ export default function DogDetails({ dog }) {
         <p>{dog.breed}</p>
         <p>{dog.age}</p>
         <p>{dog.status}</p>
+        <p>{dog.description}</p>
+        <p>{dog.contact}</p>
+        <button onClick={() => navigate(-1)}>Back to overview</button>
       </article>
     </>
   );
