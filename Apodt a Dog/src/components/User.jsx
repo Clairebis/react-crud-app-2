1c38c5c38793aad657678701edad50dc4eb3b0d3
Claire <eaaclhb@students.eaaa.dk>
2023-10-06 09:59:36 +0200

    Fix user fetch (uid)

--- Apodt a Dog/src/components/User.jsx
+++ Apodt a Dog/src/components/User.jsx
@@ -4,7 +4,7 @@ export default function User({ uid }) {
   const [user, setUser] = useState({});
   useEffect(() => {
     async function getUser() {
-      const url = `https://dog-app-7792f-default-rtdb.europe-west1.firebasedatabase.app/users`;
+      const url = `https://dog-app-7792f-default-rtdb.europe-west1.firebasedatabase.app/users/${uid}.json`;
       console.log(url);
       const response = await fetch(url);
       const data = await response.json();
--- Apodt a Dog/src/pages/NewDog.jsx
+++ Apodt a Dog/src/pages/NewDog.jsx
@@ -1,3 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function NewDog() {
-  return <div>NewDog</div>;
+  const [name, setName] = useState("");
+  const [breed, setBreed] = useState("");
+  const [age, setAge] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [contact, setContact] = useState("");
+  const [status, setStatus] = useState("");
+
+  const newDog = {
+    name: name,
+    breed: breed,
+    age: age,
+    description: description,
+    image: image,
+    contact: contact,
+    status: status,
+    uid: "fTs84KRoYw5pRZEWCq2Z",
+  };
+
+  const navigate = useNavigate();
+
+  const placeholderImage =
+    "https://archive.org/download/placeholder-image/placeholder-image.jpg";
+
+  console.log(newDog);
+
+  async function createDog(event) {
+    event.preventDefault();
+
+    newDog.uid = "fTs84KRoYw5pRZEWCq2Z";
+
+    const url =
+      "https://dog-app-7792f-default-rtdb.europe-west1.firebasedatabase.app/dogs.json";
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newDog),
+    });
+
+    if (response.ok) {
+      console.log("Dog added");
+      navigate("/");
+    } else {
+      console.log("An error occurred when adding the dog");
+    }
+  }
+
+  return (
+    <section>
+      <h1>Add a Dog</h1>
+      <form onSubmit={createDog}>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Enter the dog's name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+    </section>
+  );
 }
